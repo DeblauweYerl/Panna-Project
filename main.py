ede880d7d85a4c1b713b03f12374322a8eb19b50
Yerl <yerldeblauwe@student.howest.be>
2021-01-05 13:32:38 +0100

    minor fixes

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ leds = [Led([26, 19]), Led([13, 6]), Led([5, 0]), Led([11, 9]), Led([21, 20]), L
 buttons = [Button(10), Button(22), Button(27), Button(17), Button(24), Button(23), Button(18), Button(15)]
 bases = []
 for led in leds:
-    bases += Base(led, buttons[leds.index(led)])
+    bases.append(Base(led, buttons[leds.index(led)]))
     
 time_score = 0
-playing = False
+playing = 0
 
 def timer():
     global time_score
@@ -42,25 +42,26 @@ def singleplayer(total_bases):
 
 def multiplayer():
     for led in leds[0:4]:
-        led.activate("red")
+        led.activate('red')
     for led in leds[4:8]:
-        led.activate("blue")
+        led.activate('blue')
+
     result=" "
     while(result==" "):
-        if bases[0].active==False and bases[1]==False and bases[2]==False and bases[3]==False:
+        if bases[0].active==False and bases[1].active==False and bases[2].active==False and bases[3].active==False:
             result="blue"
     
-        if bases[4]==False and bases[5]==False and bases[6]==False and bases[7]==False:
+        if bases[4].active==False and bases[5].active==False and bases[6].active==False and bases[7].active==False:
             result="red"
 
 
 def game(gamemode):
-    if gamemode=="Multiplayer":
+    if gamemode=="multiplayer":
         name_player1=str(input("Whats the name of player 1 \n"))
         name_player2=str(input("Whats the name of player 2 \n"))
         mp = threading.Thread(target=multiplayer)
         mp.start()
-    if gamemode=="Singleplayer":
+    if gamemode=="singleplayer":
         name_player1=str(input("Whats the name of the player \n"))
         sp = threading.Thread(target=singleplayer)
         sp.start()
@@ -70,7 +71,7 @@ def game(gamemode):
 
 
 try:
-    game(gamemode)
+    game("multiplayer")
 
 except KeyboardInterrupt:
     print("\nManually stopped program")
