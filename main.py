1d0ba9587113b060952fa142aba5fe5131fb0f0c
Yerl <yerldeblauwe@student.howest.be>
2021-01-05 10:41:54 +0100

    added timer thread

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from RPi import GPIO
 import time
 import random
+import threading
 
 from models.Led import Led
+from models.Button import Button
+from models.Base import Base
 GPIO.setmode(GPIO.BCM)
 
 leds = [Led([26, 19]), Led([13, 6]), Led([5, 0]), Led([11, 9]), Led([21, 20]), Led([16, 12]), Led([1, 7]), Led([8, 25])]
@@ -12,7 +15,14 @@ for led in leds:
     bases += Base(led, buttons[leds.index(led)])
 
 
+def timer():
+    start = time.time()
+    elapsed_time = time.time() - start
+
 def singleplayer(total_bases):
+    timer_thread = threading.Thread(target=timer)
+    timer_thread.start()
+
     bases_completed = 0
     while(bases_completed <= total_bases):
         current_base = bases[random.randint(0, 7)]
