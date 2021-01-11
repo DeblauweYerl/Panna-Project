7d33d4bc53d331d2ec7db5be2729524f870fce23
Ian Remy <ianremy@student.howest.be>
2021-01-11 14:00:52 +0100

    veel

--- website/scripts/script.js
+++ website/scripts/script.js
@@ -2,43 +2,65 @@
 let naam, button;
 const lanIP = `${window.location.hostname}:5010`; //ip adres automatisch opvragen met ${window.location.hostname}
 const socketio = io(lanIP);
-let btn_start_singleplayer, singleplayer_naam, singleplayer_moeilijkheidgraad;
+let btn_start_singleplayer, singleplayer_naam, singleplayer_moeilijkheidgraad, singleplayer_tijd, btn_stop_singleplayer;
 
 
-
-const listenToClick=function(){
-    btn_start_singleplayer.addEventListener("mouseup", function() {
-
-        console.log("singleplayer starten");
+const listenToSingleplayer=function(){
+    btn_start_singleplayer.addEventListener("click", function() {
+        //singleplayer starten
         singleplayer_naam= document.querySelector(".js_singleplayer_naam").value;
-        singleplayer_moeilijkheidgraad= document.getElementById("js-singleplayer-select").options[document.getElementById("js-singleplayer-select").selectedIndex].value;;
+        singleplayer_moeilijkheidgraad= document.getElementById("js-singleplayer-select").options[document.getElementById("js-singleplayer-select").selectedIndex].value;
         socketio.emit("F2B_start_singleplayer", {sp_naam: singleplayer_naam, sp_moeilijkheidsgraad: singleplayer_moeilijkheidgraad});
         console.log(singleplayer_moeilijkheidgraad);
-        window.location.href="singleplayerGame.html"
-        
-
-    })
-    
-}
+        window.location.href="singleplayerGame.html";
+    })  
+};
 
 
+const listenToSingleplayerGame=function(){
+    console.log("test")
+    btn_stop_singleplayer.addEventListener("click", function() {
+        // tijd ophalen van de timer
+        singleplayer_tijd = document.querySelector(".time").textContent;
+        console.log(singleplayer_tijd);
+
+        // navigeren naar endgameSingleplayer.html
+        window.location.href="endgameSingleplayer.html";
+        document.querySelector('.js-end-time').innerHTML = `Je hebt het spel voltooid in ${singleplayer_tijd}`
+    })  
+};
 
 const loadSocketListeners = function () {
-    console.log("done")
+    console.log("done");
     socketio.on("message",function(msg){
-     console.log("printing message from backend")
+     console.log("printing message from backend");
      document.querySelector('.js-messages').innerHTML+= `${msg}<br>`;
     });
+
     socketio.on('B2F_client_connected',function(msg){
-      console.log(`Server Responded:${msg}`)
-      
+      console.log(`Server Responded:${msg}`);
+    });
+
+    socketio.on('B2F_stop_game',function(msg){
+        console.log(`game stoppen`);
+        // tijd stoppen
+        document.getElementById("pauseButton").click(); // Click on the button to stop
+        // tijd ophalen van de timer
+        singleplayer_tijd = document.querySelector(".time").textContent;
+        console.log(singleplayer_tijd);
+        socketio.emit("F2B_tijd", {sp_tijd: singleplayer_tijd});
+
+        // navigeren naar endgameSingleplayer.html
+        window.location.href="endgameSingleplayer.html";
+        document.querySelector('.js-end-time').innerHTML = `Je hebt het spel voltooid in ${singleplayer_tijd}`
     });
 };
 
 const checkValues = function () {
     if (naam.value.length >0 && naam.value.length <= 14) {
         button.disabled = false;
-        listenToClick();
+        //word meerdere keren opgeroepen door input
+        
     } else {
         button.disabled = true;
     }
@@ -51,12 +73,19 @@ const eventListenersToevoegen = function () {
 const init = function () {
     naam = document.querySelector('#name');
     button = document.querySelector('input[type=button]');
-    loadSocketListeners()
+    btn_start_singleplayer = document.querySelector('.js-start-singleplayer');
+    btn_stop_singleplayer = document.querySelector('.js-stop-game-singleplayer');
+    loadSocketListeners();
     button.disabled = 'disabled';
     eventListenersToevoegen();
+    if(btn_start_singleplayer!=null){
+        listenToSingleplayer();
+    }
+    if(btn_stop_singleplayer!=null){
+        listenToSingleplayerGame();
+    }
 };
 
 document.addEventListener('DOMContentLoaded', function () {
     init();
-    btn_start_singleplayer = document.querySelector('.js-start-singleplayer');
 });
