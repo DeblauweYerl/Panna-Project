a92181bb7f3e169e3f2dd11b99d78d47781ba641
Yerl <yerldeblauwe@student.howest.be>
2021-01-11 18:29:51 +0100

    fixed database connection

--- database/repositories/Datarepository.py
+++ database/repositories/Datarepository.py
@@ -1,4 +1,4 @@
-from Database import Database
+from .Database import Database
 
 class DataRepository:
     @staticmethod
@@ -12,13 +12,13 @@ class DataRepository:
 
     @staticmethod
     def read_scoreboard(difficulty):
-        sql = "SELECT DISTINCT PlayerName, Time FROM tblGame WHERE Difficulty = %s ORDER BY Time DESC"
+        sql = "SELECT DISTINCT PlayerName, Time FROM tblgame WHERE Difficulty = %s ORDER BY Time DESC"
         params = [difficulty]
         return Database.get_rows(sql, params)
 
 
     @staticmethod
     def insert_game(timestamp, player_name, difficulty, time):
-        sql = "INSERT INTO tblGame VALUES (%s,%s,%s,%s)"
+        sql = "INSERT INTO tblgame VALUES (%s,%s,%s,%s)"
         params = [timestamp, player_name, difficulty, time]
         return Database.execute_sql(sql, params)
