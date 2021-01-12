aeae916b8b1de3fc2d74937e02aa573d1b051a64
Casper-Cornelis <casper.cornelis@sgsintpaulus.eu>
2021-01-12 14:45:24 +0100

    be

--- socketio/app.py
+++ socketio/app.py
@@ -34,8 +34,8 @@ def handle_message_tijd(data):
 @socketio.on('F2B_ledselection')
 def handle_message_ledsselection(data):
     number_led=data["led"]
-    stop=data["stop"]
-    singleplayer_extra_modi(number_led,stop)
+    print(number_led)
+    #singleplayer_extra_modi(number_led,stop)
 
 
 def singleplayer_extra_modi(number_led,stop):
