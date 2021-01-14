fb1e451a1110a2cc0518175decf84028dae498a5
Yerl <yerldeblauwe@student.howest.be>
2021-01-14 16:02:23 +0100

    added invalid difficulty handling in app.py

--- database/app.py
+++ database/app.py
@@ -1,17 +1,20 @@
 from flask import Flask, request, jsonify
-from flask_CORS import CORS
+from flask_cors import CORS
 
 from repositories.Datarepository import DataRepository
 
 
 app = Flask(__name__)
-#endpoints
 endpoint = '/api'
 
 @app.route(endpoint + '/scoreboard/<difficulty>', methods=['GET'])
 def get_scoreboard(difficulty):
-    data = DataRepository.read_scoreboard(difficulty)-
-    return jsonify(data), 200
+    if difficulty in ["-1", "0", "1", "2"]:
+        data = DataRepository.read_scoreboard(difficulty)
+        return jsonify(data), 200
+    else:
+        return jsonify(message="invalid difficulty"), 400
+
 
 
 if __name__ == '__main__':
